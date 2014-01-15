c09df6404bf115f1bfdf8c548c8479bbaeb9bc8a
Guan Qing <kuno.the.developer@gmail.com>
2014-01-15 17:22:14 +0800

    Try to use curl to install

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+NODE_BIN=`which node`
+NPM_BIN=`which npm`
+
+if [[ ! ${NODE_BIN} ]];then
+    echo "node not found"
+    exit 1
+fi
+
+if [[ ! ${NPM_BIN} ]];then
+    echo "npm not found"
+    exit 1
+fi
+
+git clone https://gist.github.com/8430606.git /opt/whc
+
+cp /opt/whc-server /etc/init.d/whc-server
