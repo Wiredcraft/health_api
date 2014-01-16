f1a1bda2890addd7f55015c27028074662789fe5
Guan Qing <kuno.the.developer@gmail.com>
2014-01-16 13:55:16 +0800

    Typo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WHI_REPO=git@github.com:Wiredcraft/health_api.git
+WHI_REPO="git@github.com:Wiredcraft/health_api.git"
 WHI_ROOT=/opt/whi
 WHI_SCRIPT=${WHI_ROOT}/whi-server
 WHI_DAEMON=/etc/init.d/whi-server
@@ -8,20 +8,20 @@ NODE_BIN=`which node`
 NPM_BIN=`which npm`
 
 if [[ ! ${NODE_BIN} ]];then
-    echo "node not found"
-    exit 1
+  echo "node not found"
+  exit 1
 fi
 
 if [[ ! ${NPM_BIN} ]];then
-    echo "npm not found"
-    exit 1
+  echo "npm not found"
+  exit 1
 fi
 
 if [ ! -d ${WHI_ROOT}/.git ];then
-    git ${WHI_REPO} ${WHI_ROOT}
+  git clone ${WHI_REPO} ${WHI_ROOT}
 else
-    cd ${WHI_ROOT}
-    git pull
+  cd ${WHI_ROOT}
+  git pull
 fi
 
 cp ${WHI_SCRIPT} ${WHI_DAEMON}
